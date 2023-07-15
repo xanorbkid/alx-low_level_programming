80613df4abe3f02f6d0d40de3b7eb35cc080eee1
xanorbkid <corps.bronax@gmail.com>
2023-07-14 17:35:23 -0700

    C project tasks

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include < stdio.h >
+/**
+ * main - A simple program that prints a line with put function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multiligual puzzle");
+	return (0);
+}
